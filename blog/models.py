3275cb73b0326cb38ab48734897a350832fcbbc0
MahdiMds-Sholex <59086060+MahdiMds-Sholex@users.noreply.github.com>
2020-07-26 12:07:03 +0430

    Update models.py
    Add comment model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
+class Comment(models.Model):
+    content = models.TextField(max_length=700)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             models.SET_NULL,
+                             null=True,
+                             blank=True)
+    article = models.ForeignKey(Article, models.SET_NULL, null=True, blank=True)
+    reply_to = models.ForeignKey('self',
+                                 models.SET_NULL,
+                                 null=True,
+                                 blank=True,
+                                 related_name="replytocomment")
+    created_date = models.DateTimeField(auto_now_add=True)
